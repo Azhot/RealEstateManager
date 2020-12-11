5f5216eb1617ef8eb6bf8940456f776385a80422
Azhot <francois.jouvelot@gmail.com>
2020-12-11 11:18:17 +0100

    Added a getRealtorList function.

--- app/src/main/java/fr/azhot/realestatemanager/database/dao/RealtorDao.kt
+++ app/src/main/java/fr/azhot/realestatemanager/database/dao/RealtorDao.kt
@@ -1,10 +1,15 @@
 package fr.azhot.realestatemanager.database.dao
 
 import androidx.room.*
+import fr.azhot.realestatemanager.model.Property
 import fr.azhot.realestatemanager.model.Realtor
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface RealtorDao {
+
+    @Query("SELECT * FROM realtor_table")
+    fun getRealtorList(): Flow<List<Realtor>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insertRealtor(realtor: Realtor)
--- app/src/main/java/fr/azhot/realestatemanager/repository/RealtorRepository.kt
+++ app/src/main/java/fr/azhot/realestatemanager/repository/RealtorRepository.kt
@@ -1,9 +1,15 @@
 package fr.azhot.realestatemanager.repository
 
 import fr.azhot.realestatemanager.database.dao.RealtorDao
+import fr.azhot.realestatemanager.model.Property
 import fr.azhot.realestatemanager.model.Realtor
+import kotlinx.coroutines.flow.Flow
 
 class RealtorRepository(private val realtorDao: RealtorDao) {
+
+    // variables
+    val realtorList: Flow<List<Realtor>> = realtorDao.getRealtorList()
+
 
     // functions
     suspend fun insertRealtor(realtor: Realtor) {
