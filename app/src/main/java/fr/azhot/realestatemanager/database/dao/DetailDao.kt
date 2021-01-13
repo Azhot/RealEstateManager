bcb32ea24316a09f4797da9fefd1916848a24cfe
Azhot <francois.jouvelot@gmail.com>
2021-01-13 14:23:28 +0100

    Started implementing filtering via SQL query.

--- app/src/main/java/fr/azhot/realestatemanager/database/dao/DetailDao.kt
+++ app/src/main/java/fr/azhot/realestatemanager/database/dao/DetailDao.kt
@@ -1,16 +1,45 @@
 package fr.azhot.realestatemanager.database.dao
 
+import androidx.lifecycle.LiveData
 import androidx.room.*
 import fr.azhot.realestatemanager.model.Detail
+import fr.azhot.realestatemanager.model.MinMax
 import fr.azhot.realestatemanager.model.Property
+import fr.azhot.realestatemanager.model.PropertyType
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface DetailDao {
 
     @Transaction
-    @Query("SELECT * FROM detail_table")
-    fun getPropertyList(): Flow<List<Property>>
+    @Query(
+        """SELECT * FROM detail_table d
+                WHERE (:propertyType IS NULL or d.propertyType = :propertyType)
+                AND (:minPrice IS NULL or d.price >= :minPrice)
+                AND (:maxPrice IS NULL or d.price <= :maxPrice)
+                AND (:minSquareMeters IS NULL or d.squareMeters >= :minSquareMeters)
+                AND (:maxSquareMeters IS NULL or d.squareMeters <= :maxSquareMeters)
+                AND (:minRooms IS NULL or d.rooms >= :minRooms)
+                AND (:maxRooms IS NULL or d.rooms <= :maxRooms)"""
+    )
+    fun getPropertyFilterableList(
+        propertyType: PropertyType?,
+        minPrice: Int?,
+        maxPrice: Int?,
+        minSquareMeters: Int?,
+        maxSquareMeters: Int?,
+        minRooms: Int?,
+        maxRooms: Int?,
+    ): Flow<List<Property>>
+
+    @Query("SELECT MIN(price) as min, MAX(price) as max FROM detail_table")
+    fun getPriceBounds(): LiveData<MinMax>
+
+    @Query("SELECT MIN(squareMeters) as min, MAX(squareMeters) as max FROM detail_table")
+    fun getSquareMetersBounds(): LiveData<MinMax>
+
+    @Query("SELECT MIN(rooms) as min, MAX(rooms) as max FROM detail_table")
+    fun getRoomsBounds(): LiveData<MinMax>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insertDetail(detail: Detail)
